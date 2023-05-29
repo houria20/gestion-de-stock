9f8f1b4577a313d233ad8897cbca3a46cfd86dba
houria20 <houria.marref20@gmail.com>
2023-05-29 19:55:04 +0100

    1st commit

--- gestio-de-stock/src/main/java/com/demo/repository/ArticleRepository.java
+++ gestio-de-stock/src/main/java/com/demo/repository/ArticleRepository.java
@@ -8,11 +8,10 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class ArticleRepository implements PanacheRepository<Article> {
-    public void save(Article article){
-        System.out.println("hello world");
-        System.out.println("tva--"+article.getTauxTva());
+    public void save(Article article) {
         getEntityManager().merge(article);//persist(article);
     }
+
     public Optional<Article> findByCodeArticle(String codeArticle) {
         return find("codeArticle", codeArticle).firstResultOptional();
     }
